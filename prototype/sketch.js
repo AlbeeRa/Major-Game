ba865d8d4706738b2d630f763a658afaafd4590e
AlbeeRa <ar.senpai03@gmail.com>
2023-06-06 01:25:34 -0600

    figured out Matter.js library(yipee!)

--- prototype/sketch.js
+++ prototype/sketch.js
@@ -1,5 +1,5 @@
 // Albee Raida
-// Date
+// DRAFT 1
 //angry bird protopype.
 //https://himco.jp/2019/04/03/14%EF%BC%9A%E3%82%B2%E3%83%BC%E3%83%A0%E5%88%B6%E4%BD%9C%E3%81%AB%E5%BD%B9%E7%AB%8B%E3%81%A4p5-play%E9%80%9F%E7%BF%92-creative-coding-javascript/#%E7%A7%BB%E5%8B%95%E3%81%99%E3%82%8B%E3%82%B9%E3%83%97%E3%83%A9%E3%82%A4%E3%83%88
 // let spr;
@@ -9,89 +9,89 @@
 
 //https://www.youtube.com/watch?v=KiC7gyE4IwM
 //https://www.youtube.com/watch?v=AXfeZr2ISTs&list=PLQgDWpXcKGAe3R6oLy5pfdrXn-poA4-uu
-let bird;
-let startX = '';
-let startY ='';
+// let bird;
+// let startX = '';
+// let startY ='';
 
-let stringStartX= ''; 
-let stringStartY ='';
-let slingshot;
+// let stringStartX= ''; 
+// let stringStartY ='';
+// let slingshot;
 
-function preload(){
-  cat = loadImage("cat.png");
-}
+// function preload(){
+//   cat = loadImage("cat.png");
+// }
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  world.gravity.y = 20;
-  startX = windowWidth/10;
-  startY =windowHeight/10;
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   world.gravity.y = 20;
+//   startX = windowWidth/10;
+//   startY =windowHeight/10;
 
-  stringStartX= windowWidth/6;
-  stringStartY = 2*windowHeight/2.5;
+//   stringStartX= windowWidth/6;
+//   stringStartY = 2*windowHeight/2.5;
 
-  let boxY = 500;
-  for (let i = 0; i<5; i++){
-    boxY = boxY-50;
-    new Sprite (4*windowWidth/5,boxY,100,100);
-    Sprite.color = 'yellow';
-  }
-
-  slingshot = new Sprite(stringStartX,stringStartY, 20, windowHeight/10,"n");
-
-  bird = new Sprite(startX,startY);
-  bird.diameter = 50;
-  let floor = new Sprite(0,height,width*2,10,"static");
-}
-
-
-
-
-
-function draw() {
-  background(255);
-  if(bird.mouse.dragging()){
-    let distance = dist(bird.x,bird.y,stringStartX,stringStartY);
-    if(distance<120){
-      line(stringStartX*windowWidth/10,stringStartY*windowHeight/10,bird.x,bird.y);
-      bird.collider = "k";
-      bird.moveTowards(mouse.x+bird.mouse.x,mouse.y +bird.mouse.y,1);
-    }
-    else{
-      bird.x=startX;
-      bird.y=startY;
-      bird.collider = "k";
-    }
-  }
-}
-
-//   if(bird.x<0){ //check left wall
-//     bird.direction = bird.direction*-1;
+//   let boxY = 500;
+//   for (let i = 0; i<5; i++){
+//     boxY = boxY-50;
+//     new Sprite (4*windowWidth/5,boxY,100,100);
+//     Sprite.color = 'yellow';
 //   }
-//   if(bird.x>windowWidth){ //check right
-//     bird.direction = bird.direction*-1;
-//   }
-//   if(bird.y<0){ //check top wall
-//     bird.direction = bird.direction*-1;
-//   }
-//   if(bird.y>windowHeight){ //check bottom
-//     bird.direction = bird.direction*-1;
+
+//   slingshot = new Sprite(stringStartX,stringStartY, 20, windowHeight/10,"n");
+
+//   bird = new Sprite(startX,startY);
+//   bird.diameter = 50;
+//   let floor = new Sprite(0,height,width*2,10,"static");
+// }
+
+
+
+
+
+// function draw() {
+//   background(255);
+//   if(bird.mouse.dragging()){
+//     let distance = dist(bird.x,bird.y,stringStartX,stringStartY);
+//     if(distance<120){
+//       line(stringStartX*windowWidth/10,stringStartY*windowHeight/10,bird.x,bird.y);
+//       bird.collider = "k";
+//       bird.moveTowards(mouse.x+bird.mouse.x,mouse.y +bird.mouse.y,1);
+//     }
+//     else{
+//       bird.x=startX;
+//       bird.y=startY;
+//       bird.collider = "k";
+//     }
 //   }
 // }
 
-if(mouse.released()){
-  bird.direction = bird.angleTo(slingshot);
-  bird.vel.x = (stringStartX - bird.x)/7;
-  bird.vel.y = (stringStartY -bird.y)/7;
-  bird.mass = 30;
-  bird.collider = "d";
-}
+// //   if(bird.x<0){ //check left wall
+// //     bird.direction = bird.direction*-1;
+// //   }
+// //   if(bird.x>windowWidth){ //check right
+// //     bird.direction = bird.direction*-1;
+// //   }
+// //   if(bird.y<0){ //check top wall
+// //     bird.direction = bird.direction*-1;
+// //   }
+// //   if(bird.y>windowHeight){ //check bottom
+// //     bird.direction = bird.direction*-1;
+// //   }
+// // }
+
+// if(mouse.released()){
+//   bird.direction = bird.angleTo(slingshot);
+//   bird.vel.x = (stringStartX - bird.x)/7;
+//   bird.vel.y = (stringStartY -bird.y)/7;
+//   bird.mass = 30;
+//   bird.collider = "d";
+// }
 
 
 
-//THINGS TO DO!!
-//pointing system :(when the bird touches the brick, it turns red (disapears) and adds 1 to counter
-//which will be displayed in the left corner)
+// //THINGS TO DO!!
+// //pointing system :(when the bird touches the brick, it turns red (disapears) and adds 1 to counter
+// //which will be displayed in the left corner)
 
 
 
--- sketch.js
+++ sketch.js
@@ -4,150 +4,216 @@
 //
 // Extra for Experts:
 // 
-//add music links
-//https://www.youtube.com/watch?v=o9_Gu3TI4IY or https://www.youtube.com/watch?v=hAAlDoAtV7Y
 
+// eslint-disable-next-line no-var
+let Engine = Matter.Engine;
+//let Render = Matter.Render;
+let Runner = Matter.Runner;
+let Bodies = Matter.Bodies;
+let Composite = Matter.Composite;
 
-let mode; // to determine the game has started or not
+// create an engine
+let engine; 
+let world;
+let boxes=[];
+let runner;
 
-//image
-let sky;
-let city;
-let alien;
-let kitty;
-
-let myFont;
 let ground;
-let box;
-let cat;
 
-class Box{
-  constructor(x,y,w,h){
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.groundColour = color("#082226");
-  }
-  //ground
-  displayGround(){
-    fill(this.groundColour);
-    rect(this.x,this.y,this.w,this.h);
-  }
-  //aliens
-  display(){
-    image(alien,windowWidth/1.5,(windowHeight/2.4),width/4,height/2);
-  }
-}
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-class Cat{
-  constructor(x,y,r){
-    this.x = x;
-    this.y = y;
-    this.r = r;
-  }
-  displayCat(){
-    fill(255);
-    image(kitty,windowWidth/7,(windowHeight/1.5),width/8,height/5);
+function setup(){
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
 
-  }
+  let option ={
+    isStatic:true
+  };
+  ground = Bodies.rectangle(200,height,width,100,option);
+  Composite.add(engine.world,[ground]);
+  runner = Runner.create();
+  Runner.run(runner, engine);
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function preload(){
-  sky = loadImage("clouds.png");
-  city = loadImage("cityscape.png");
-  alien = loadImage("alien.png");
-  kitty = loadImage("cat.png");
-  //myFont = loadFont("ClassicRock.ttf");
+function draw(){
+  background(51);
+  for(let i = 0;i<boxes.length;i++){
+    boxes[i].show();
+  }
+  noStroke();
+  fill(170);
+  strokeWeight(3);
+  rectMode(CENTER);
+  rect(ground.position.x,ground.position.y,width,100);
 }
 
-let playerSprite;
-function setup() {
-  mode = 0;
-  createCanvas(windowWidth, windowHeight);
-
-
-  ground = new Box(0,windowHeight/1.2,windowWidth,200);
-  box = new Box(alien,this.x,this.y,this.w, this.h);
-  cat = new Cat(kitty,this.x,this.y,this.w,this.h);
-  textSize(60);
+function mousePressed(){
+  boxes.push(new Box(mouseX,mouseY,random(10,40),random(10,40)));
 }
 
-function draw() {
-  background(0);
-  mainMenu();
+function Box(x,y,w,h){
+  let options={
+    friction:0.3,
+    restitution: 0.6
+  };
+  this.body = Bodies.rectangle(x, y, w, h,options);
+  this.w= w;
+  this.h= h;
+  Composite.add(engine.world,[this.body]);
 
-  //testing
-  box.display();
-  ground.displayGround();
-  cat.displayCat();
+  this.show = function(){
+    let pos = this.body.position;
+    let angle = this.body.angle;
 
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    push();
+    translate(pos.x,pos.y);
+    rectMode(CENTER);
+    rect(0,0,this.w,this.h);
 
-//idea: raining cats or cats moving in rectangle
-function mainMenu(){
-
-  //first thing you will see
-  if (mode===0){
-    background(sky);
-
-    //title
-    textAlign(CENTER);
-    fill("black");
-    text("Pawtactor", windowWidth/2,windowHeight/3);
-
-    //start command
-    fill("#fff5eb");
-    text("Press Enter", windowWidth/2,windowHeight/2);
-    //textFont(myFont);
-///////////////////////////////////////////////////////
-
-
-
-
-
-
-  }
-  //How to play
-  if (mode=== 1 ){
-
-    //tutorial
-    fill("#ffffff");
-    text("press shift to start", windowWidth/2,windowHeight/2);
-    text("press esc to return", windowWidth/2,windowHeight/1.1);
-
-    textSize(40);
-    fill("#fff5eb");
-    text("use your mouse to control the cat", windowWidth/2,windowHeight/3);
-  }
-
-  if (mode === 2 ){ //Game start
-    pawtactor();
-  }
+    pop();
+  };
 
 }
 
-function keyPressed() {
-  if (keyCode === ENTER) { // tutorial
-    mode= 1;
-  }
-  if (keyCode === ESCAPE) {// go back to the main menu
-    mode= 0;
-  }
-  if (keyCode === SHIFT) {// start of the game
-    mode= 2;
-  }
-}
+// let mode; // to determine the game has started or not
+
+// //image
+// let sky;
+// let city;
+// let alien;
+// let kitty;
+
+// let myFont;
+// let ground;
+// let box;
+// let cat;
+
+// class Box{
+//   constructor(x,y,w,h){
+//     this.x = x;
+//     this.y = y;
+//     this.w = w;
+//     this.h = h;
+//     this.groundColour = color("#082226");
+//   }
+//   //ground
+//   displayGround(){
+//     fill(this.groundColour);
+//     rect(this.x,this.y,this.w,this.h);
+//   }
+//   //aliens
+//   display(){
+//     image(alien,windowWidth/1.5,(windowHeight/2.4),width/4,height/2);
+//   }
+// }
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// class Cat{
+//   constructor(x,y,r){
+//     this.x = x;
+//     this.y = y;
+//     this.r = r;
+//   }
+//   displayCat(){
+//     fill(255);
+//     image(kitty,windowWidth/7,(windowHeight/1.5),width/8,height/5);
+
+//   }
+// }
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// function preload(){
+//   sky = loadImage("clouds.png");
+//   city = loadImage("cityscape.png");
+//   alien = loadImage("alien.png");
+//   kitty = loadImage("cat.png");
+//   //myFont = loadFont("ClassicRock.ttf");
+// }
+
+// let playerSprite;
+// function setup() {
+//   mode = 0;
+//   createCanvas(windowWidth, windowHeight);
 
 
-// game coding
-function pawtactor(){
-  background(city);
-  box.display();
-  ground.displayGround();
-  cat.displayCat();
-  //cat.moveTowards(mouse, 0.10);
-}
+//   ground = new Box(0,windowHeight/1.2,windowWidth,200);
+//   box = new Box(alien,this.x,this.y,this.w, this.h);
+//   cat = new Cat(kitty,this.x,this.y,this.w,this.h);
+//   textSize(60);
+// }
+
+// function draw() {
+//   background(0);
+//   mainMenu();
+
+//   //testing
+//   box.display();
+//   ground.displayGround();
+//   cat.displayCat();
+
+// }
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// //idea: raining cats or cats moving in rectangle
+// function mainMenu(){
+
+//   //first thing you will see
+//   if (mode===0){
+//     background(sky);
+
+//     //title
+//     textAlign(CENTER);
+//     fill("black");
+//     text("Pawtactor", windowWidth/2,windowHeight/3);
+
+//     //start command
+//     fill("#fff5eb");
+//     text("Press Enter", windowWidth/2,windowHeight/2);
+//     //textFont(myFont);
+// ///////////////////////////////////////////////////////
+
+
+
+
+
+
+//   }
+//   //How to play
+//   if (mode=== 1 ){
+
+//     //tutorial
+//     fill("#ffffff");
+//     text("press shift to start", windowWidth/2,windowHeight/2);
+//     text("press esc to return", windowWidth/2,windowHeight/1.1);
+
+//     textSize(40);
+//     fill("#fff5eb");
+//     text("use your mouse to control the cat", windowWidth/2,windowHeight/3);
+//   }
+
+//   if (mode === 2 ){ //Game start
+//     pawtactor();
+//   }
+
+// }
+
+// function keyPressed() {
+//   if (keyCode === ENTER) { // tutorial
+//     mode= 1;
+//   }
+//   if (keyCode === ESCAPE) {// go back to the main menu
+//     mode= 0;
+//   }
+//   if (keyCode === SHIFT) {// start of the game
+//     mode= 2;
+//   }
+// }
+
+
+// // game coding
+// function pawtactor(){
+//   background(city);
+//   box.display();
+//   ground.displayGround();
+//   cat.displayCat();
+//   //cat.moveTowards(mouse, 0.10);
+// }
 
